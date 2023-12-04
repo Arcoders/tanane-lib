bfda463d5257d862f055b3af6c604871a3fd95c9
arcoders <ismaelhaytamtanane@gmail.com>
2023-12-04 22:05:09 +0100

    fix alert import

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,11 +1,13 @@
 import { Button } from "./Button";
-import { Alert } from "./Alert";
+import { Alert, useAlert, AlertProvider } from "./Alert";
 import { Pagination, usePagination } from "./Pagination";
 import { Table } from "./Table";
 import classnames from "./utils/classnames";
 
 export {
     Alert,
+    useAlert, 
+    AlertProvider,
     Table,
     Button,
     Pagination,
